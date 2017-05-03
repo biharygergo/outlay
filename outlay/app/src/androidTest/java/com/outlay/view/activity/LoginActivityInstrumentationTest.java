ba881c55b5c074e83ed981b4303028691cee2f9a
Dávid Ádám <95david.adam@gmail.com>
2017-05-04 00:10:21 +0200

    Sign Up UI tests added #13

--- outlay/app/src/androidTest/java/com/outlay/view/activity/LoginActivityInstrumentationTest.java
+++ outlay/app/src/androidTest/java/com/outlay/view/activity/LoginActivityInstrumentationTest.java
@@ -4,6 +4,7 @@ import android.os.SystemClock;
 import android.support.annotation.IdRes;
 import android.support.test.espresso.ViewInteraction;
 import android.support.test.rule.ActivityTestRule;
+import android.view.KeyEvent;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -15,6 +16,7 @@ import org.junit.Test;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.pressKey;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isAssignableFrom;
@@ -59,6 +61,50 @@ public class LoginActivityInstrumentationTest {
         EspressoTextInputLayoutUtils.onEditTextWithinTilWithId(R.id.signInInputEmail).perform(typeText("aaaa@aaa.hu"));
         EspressoTextInputLayoutUtils.onEditTextWithinTilWithId(R.id.signInInputPassword).perform(typeText("Bela123"));
         onView(withText(R.string.label_sign_in)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void validateSignUpEmailField(){
+        onView(withId(R.id.fab)).perform(click());
+        onView(withId(R.id.signUpEmail)).perform(typeText("test@email.com")).check(matches(withText("test@email.com")));
+    }
+
+    @Test
+    public void validateNoSignUpEmailInput(){
+        onView(withId(R.id.fab)).perform(click());
+        onView(withId(R.id.signUpButton)).perform(click());
+        EspressoTextInputLayoutUtils.onErrorViewWithinTilWithId(R.id.signUpInputEmail).check(matches(withText("Invalid email")));
+    }
+
+    @Test
+    public void validateNoSignUpPasswordInput(){
+        onView(withId(R.id.fab)).perform(click());
+        onView(withId(R.id.signUpButton)).perform(click());
+        EspressoTextInputLayoutUtils.onErrorViewWithinTilWithId(R.id.signUpInputPassword).check(matches(withText("Invalid password. At least 6 chars")));
+    }
+
+    @Test
+    public void validateNoMatchPasswordFields(){
+        onView(withId(R.id.fab)).perform(click());
+        onView(withId(R.id.signUpPassword)).perform(typeText("Pasword"), pressKey(KeyEvent.KEYCODE_ENTER));
+        onView(withId(R.id.signUpRepeatPassword)).perform(typeText("password"), pressKey(KeyEvent.KEYCODE_ENTER));
+        onView(withId(R.id.signUpButton)).perform(click());
+        EspressoTextInputLayoutUtils.onErrorViewWithinTilWithId(R.id.signUpInputRepeatPassword).check(matches(withText("Password does not match")));
+    }
+
+    @Test
+    public void validateEmailAddressAlredyInUse(){
+        onView(withId(R.id.fab)).perform(click());
+        onView(withId(R.id.signUpEmail)).perform(typeText("test@email.com"));
+        onView(withId(R.id.signUpPassword)).perform(click(), typeText("password"), pressKey(KeyEvent.KEYCODE_ENTER));
+        onView(withId(R.id.signUpRepeatPassword)).perform(typeText("password"), pressKey(KeyEvent.KEYCODE_ENTER));
+        onView(withId(R.id.signUpButton)).perform(click());
+        onView(withText(R.string.label_sign_up)).check(matches(isDisplayed()));
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
 
